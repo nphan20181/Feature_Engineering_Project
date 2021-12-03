fca7f53c01269208f14738ca339de75434f02c3a
nagasumanth <nagasumanth.ece@gmail.com>
2021-12-02 21:02:57 -0600

    flask website final
    trained models are not uploaded coz of large file size.
    trained models can be downloaded from here - https://drive.google.com/drive/folders/1NrDU8hytxfkOB68bDNitE3T8qVUF5dEU
    (trained models are .h5 files)

--- web_app/main.py
+++ web_app/main.py
@@ -4,6 +4,9 @@ import pandas as pd
 from flask import Flask, render_template, flash, request, redirect, url_for, jsonify, Markup, send_file
 from werkzeug.utils import secure_filename
 import boto3
+import cv2
+import matplotlib.pyplot as plt
+from keras.models import load_model
 
 
 
@@ -66,45 +69,165 @@ def display_result():
     # build data frame that store result for image classification
     df_predictions = pd.DataFrame(columns=['Model', 'Predicted Flower Class', 'Probability'])
 
-    df_predictions = gabor_filter_cnn_classify(filename, df_predictions)
-    df_predictions = gaussian_filter_cnn_classify(filename, df_predictions)
-    df_predictions = sharpening_filter_cnn_classify(filename, df_predictions)
+    # load uploaded image
+    uploaded_image = plt.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # resize image
+    resize_img = cv2.resize(uploaded_image, (150,150))
+
+    #convert to np from image
+    img_as_np = np.array(resize_img)
+
+    df_predictions, x1 = gaussian_filter_cnn_classify(df_predictions, img_as_np)
+    df_predictions, x2 = gabor_filter_cnn_classify(df_predictions, img_as_np)
+    df_predictions, x3 = sharpening_filter_cnn_classify(df_predictions, img_as_np)
+    df_predictions = merged_model_ccn(df_predictions, [x1, x2, x3])
     df_predictions = aws_rekognition_classify(filename, df_predictions)
     
     return render_template('result.html', url=filename, predictions=df_predictions)
 
+def get_flower_name_from_class(class_number):
 
-def gabor_filter_cnn_classify(filename, df_predictions):
+    flower_dict = {0: "Daisy", 1: "Dandelion", 2: "Rose", 3: "Sunflower", 4: "Tulip"}
+    return flower_dict[int(class_number)]
 
+def gaussian_filter_cnn_classify(df_predictions, img_np):
 
+    #apply gaussian filter on resized image
+    gauss_feature = cv2.GaussianBlur(img_np, (3, 33), 0)
 
-    # save prediction to data frame
-    df_predictions = df_predictions.append({'Model': 'Gabor Filter CNN', \
-                                            'Predicted Flower Class': 'TO DO',\
-                                            'Probability': 'TO DO'}, ignore_index=True)
-    return df_predictions
+    #normalize gaussian feature
+    gaussx = np.array(gauss_feature)/255
 
-def gaussian_filter_cnn_classify(filename, df_predictions):
+    #convert to list, as model input is based on list.
+    images_list = []
+    images_list.append(gaussx)
+    x = np.asarray(images_list)
 
+    # invoke trained model
+    model = load_model('modelgaussbest.h5')
 
+    result = model.predict(x)
+
+    print("----gaussian model result")
+    print(result)
+
+    predicted_class = np.asscalar(np.argmax(result,axis=1))
+
+    print("predicted_class")
+    print(predicted_class)
+
+    prob = np.asscalar(result[0][predicted_class])
+    print("probability")
+    print(prob)
 
     # save prediction to data frame
     df_predictions = df_predictions.append({'Model': 'Gaussian Filter CNN', \
-                                            'Predicted Flower Class': 'TO DO',\
-                                            'Probability': 'TO DO'}, ignore_index=True)
-    return df_predictions
+                                            'Predicted Flower Class': get_flower_name_from_class(predicted_class),\
+                                            'Probability': str(prob)}, ignore_index=True)
+    return df_predictions, x
+
+def gabor_filter_cnn_classify(df_predictions, img_np):
 
 
-def sharpening_filter_cnn_classify(filename, df_predictions):
+    #apply gabor filter on resized image
+    g_kernel = cv2.getGaborKernel((13, 13), 4.0, 56.2, 10.0, 1, 0, ktype=cv2.CV_32F)
+    gabor_feature = cv2.filter2D(img_np, cv2.CV_8UC3, g_kernel)
 
+    #normalize gabor feature
+    gaborx = np.array(gabor_feature)/255
 
+    #convert to list, as model input is based on list.
+    images_list = []
+    images_list.append(gaborx)
+    x = np.asarray(images_list)
+
+    # invoke trained model
+    model = load_model('modelgaborbest.h5')
+
+    result = model.predict(x)
+
+    print("----gabor model result")
+    print(result)
+
+    predicted_class = np.asscalar(np.argmax(result,axis=1))
+
+    print("predicted_class")
+    print(predicted_class)
+
+    prob = np.asscalar(result[0][predicted_class])
+    print("probability")
+    print(prob)
+
+    # save prediction to data frame
+    df_predictions = df_predictions.append({'Model': 'Gabor Filter CNN', \
+                                            'Predicted Flower Class': get_flower_name_from_class(predicted_class),\
+                                            'Probability': str(prob)}, ignore_index=True)
+    return df_predictions, x
+
+def sharpening_filter_cnn_classify(df_predictions, img_np):
+
+    #apply sharp filter on resized image
+    skernel = np.array([[-1,-1,-1], [-1,10,-1], [-1,-1,-1]])
+    sharp_feature = cv2.filter2D(img_np, -1, skernel)
+
+    #normalize gaussian feature
+    sharpx = np.array(sharp_feature)/255
+
+    #convert to list, as model input is based on list.
+    images_list = []
+    images_list.append(sharpx)
+    x = np.asarray(images_list)
+
+    # invoke trained model
+    model = load_model('modelsharpbest.h5')
+
+    result = model.predict(x)
+
+    print("----Sharpening model result")
+    print(result)
+
+    predicted_class = np.asscalar(np.argmax(result,axis=1))
+
+    print("predicted_class")
+    print(predicted_class)
+
+    prob = np.asscalar(result[0][predicted_class])
+    print("probability")
+    print(prob)
 
     # save prediction to data frame
     df_predictions = df_predictions.append({'Model': 'Sharpening Filter CNN', \
-                                            'Predicted Flower Class': 'TO DO',\
-                                            'Probability': 'TO DO'}, ignore_index=True)
-    return df_predictions
+                                            'Predicted Flower Class': get_flower_name_from_class(predicted_class),\
+                                            'Probability': str(prob)}, ignore_index=True)
+    return df_predictions, x
 
+
+def merged_model_ccn(df_predictions, model_input):
+
+    # invoke trained model
+    model = load_model('merged_model.h5')
+
+    result = model.predict(model_input)
+
+
+    print("----Merged model result")
+    print(result)
+
+    predicted_class = np.asscalar(np.argmax(result,axis=1))
+
+    print("predicted_class")
+    print(predicted_class)
+
+    prob = np.asscalar(result[0][predicted_class])
+    print("probability")
+    print(prob)
+
+    # save prediction to data frame
+    df_predictions = df_predictions.append({'Model': 'Merged Model CNN', \
+                                            'Predicted Flower Class': get_flower_name_from_class(predicted_class),\
+                                            'Probability': str(prob)}, ignore_index=True)
+    return df_predictions
 
 
 def aws_rekognition_classify(filename, df_predictions):
@@ -112,9 +235,9 @@ def aws_rekognition_classify(filename, df_predictions):
     #initialize rekogniton client
     rekognition = boto3.client(
     'rekognition',
-    aws_access_key_id='ASIAVZKUOEYSHABXNY6B',
-    aws_secret_access_key='9n6rR4OeU4ieG0UvIIJr2o09Xc++Un0aVkfsnw18',
-    aws_session_token='IQoJb3JpZ2luX2VjELH//////////wEaCXVzLWVhc3QtMSJHMEUCIQD3gyPATfERd10/GVAR8tTv8rUgku9g9PJRP7vD+pKuDgIgXnlOM1e9OfNuSyZUnnWtmn8JeScxQj6Soj72v1ljbDcq7wEIahAAGgwzOTc5OTg0MzM4MjgiDOnT4gI52uBtYovtQyrMAdPawGT+RSgWDspWaxcK55XzQ5dDnmBnum003u10A5soPwEz7mSNcJH75MhSOZ2PHv2wBX1UPVgXYluvV6w7ppzMGsCMcNKHtN2dSXtJzWoh3ARtDOfgejBtvHZhSvpnwdpA5qV9VrGn3Bj9WeASgAL8AE8At/8ToF1PUST5FQlO1Wys8Dl5gBl4uPT/0zIAzQn54pwMQ2cq3xFyByGuqFfNdb2JiJuZczFwt3aryldWtg+7YyuywG9EMlU02xRhP6vas/65q+G69YmuJTD20MaMBjqYAT6e9Sc2hxKPovQpLUd7y3K1IC94e8nH8w8+QL0udROj/LCEhQEu4nvGDuUsc2YE8CnXD7wfY86whAOOJde9eXU4M1FUcQDDxlqCcL+sEplPd0aYKGlp4/8BPVzsG15RCE5Lntpo7TvS83nIOQzhVGMNOAzZquBcrwTcEeviQWk8lB7BuhlYdZfqD11Z1KpYSIwh2kf2FZrt')
+    aws_access_key_id='ASIA2DV7S2FXT3N7NOFE',
+    aws_secret_access_key='tKMW7heeCiLRvIdu+NtaZ0ws00u/NFfTrsIiywo1',
+    aws_session_token='IQon1a1g1a1Jb3JpZ2luX2VjEGMaCXVzLWVhc3QtMSJHMEUCIA/VatdN7fYDnPT9XfgeVqus1F9j3lrK1pKVgI6rSI5NAiEA6xRrV/IJjGBf00VuJHNe2iD/ZCSUqrs50OWKHVqlScwq7wEIOxAAGgw2OTUxMTI3NDk0MjMiDHd+90ki7ARnuYSZoyrMAb5+r+8dekdQO6VIfTaodaEgVSLdlm6ol5eI7Qolo/RNqa5bpb9CZoEOd0PcTAlVjBC0FpAuZm+FGlPLd8X1clODol3j3kiQvkY82dUiTmO8rM53V5D7euOW202PKsnNYHFp92sc9eHywQU7nhhF1bI9on0AIHl0gjx6BRCadSIEBY1LceTkNxIvj8FoH+KWrnmr06pT4v+gw7q45dbRAYjASSMNhS7qO7Lg3P9+aJMSyLpPe4Fuy5CPY75DJzXah6ChuvE+Y9PbJmKcSTCY+6WNBjqYAbT4oJ/PykFBAIZ2ykEuS0chupLkYWtP3FEmJjhAHZGmtqGY5nyD7sudksG3XnhvuIhbNXSsLWfrGzBKmai0NqYnxhe/S+efaTM2n6jjo9hwaj6PYy2CdoSPVe/B04Y4I5x56xgqyUbOBcEnQHWGHoj2vvONLA0/yEMuD+Ec7eVFKsdnhTiWlVOoZ2DnS+wt89YBp8ohxBYZ')
 
     #invoke aws rekognition api
     with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'rb') as image_data:
